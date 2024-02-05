e2e0c16f73bbe540a44610e2ebb09b1e55c625e1
KomilovAzizbekk <komilovazizbek.ppp@gmail.com>
2024-02-06 01:25:06 +0500

    MarketController.java LanguageController.java CRUD done. Bot Backend 60% Done.

---
+++ src/main/java/uz/mediasolutions/saleservicebot/controller/impl/CategoryControllerImpl.java
@@ -0,0 +1,10 @@
+package uz.mediasolutions.saleservicebot.controller.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import uz.mediasolutions.saleservicebot.controller.abs.CategoryController;
+
+@RestController
+@RequiredArgsConstructor
+public class CategoryControllerImpl implements CategoryController {
+}
