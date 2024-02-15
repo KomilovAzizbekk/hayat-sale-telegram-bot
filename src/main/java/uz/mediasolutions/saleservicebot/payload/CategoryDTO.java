cb3908fccce3e8fd125c885f83eabb9cf0a0d9eb
KomilovAzizbekk <komilovazizbek.ppp@gmail.com>
2024-02-15 12:46:21 +0500

    NameUz, NameRu, Number are made as must be given.

--- src/main/java/uz/mediasolutions/saleservicebot/payload/CategoryDTO.java
+++ src/main/java/uz/mediasolutions/saleservicebot/payload/CategoryDTO.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Data
@@ -15,10 +17,13 @@ public class CategoryDTO {
 
     private UUID id;
 
+    @NotBlank(message = "NameUz cannot be blank")
     private String nameUz;
 
+    @NotBlank(message = "NameRu cannot be blank")
     private String nameRu;
 
+    @NotNull(message = "Number cannot be null")
     private Integer number;
 
 }
--- src/main/java/uz/mediasolutions/saleservicebot/payload/MarketDTO.java
+++ src/main/java/uz/mediasolutions/saleservicebot/payload/MarketDTO.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,8 +16,10 @@ public class MarketDTO {
 
     private Long id;
 
+    @NotBlank(message = "NameUz cannot be blank")
     private String nameUz;
 
+    @NotBlank(message = "NameRu cannot be blank")
     private String nameRu;
 
 }
